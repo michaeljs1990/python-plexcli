a8536d20b00a41da9535cb0f379aa90056d776db
Michael Schuett <michaeljs1990@gmail.com>
2018-03-17 06:42:17 +0000

    Inital Commit
    First outline for the plexcli service using python 3.6 and pipenv.
    This likely doesn't run at all yet.

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+"""
+    Pip module setup
+"""
+from setuptools import setup
+from pipenv.project import Project
+from pipenv.utils import convert_deps_to_pip
+
+# Load in requirements from Pipfile
+pfile = Project(chdir=False).parsed_pipfile
+requirements = convert_deps_to_pip(pfile['packages'], r=False)
+
+setup(name='plexcli',
+      version='0.0.1',
+      description='Plex CLI for configuration from the cli',
+      url='http://github.com/michaeljs1990/plexcli',
+      author='Michael Schuett',
+      author_email='michaelj1990@gmail.com',
+      license='MIT',
+      install_requires=requirements,
+      packages=["plexcli", "plexcli/cli"],
+      entry_points={
+        'console_scripts': [
+            'plexcli=plexcli.cli.main:main',
+        ],
+},)
